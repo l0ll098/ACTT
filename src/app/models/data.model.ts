90fbd82803bc20d4a6624e8e36cecb84dd02d6be
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2018-09-08 11:57:24 +0200

    Model update

--- src/app/models/data.model.ts
+++ src/app/models/data.model.ts
@@ -1,88 +1,15 @@
-export interface QualifyingResult extends Results {
-    Q1: string;
-    Q2?: string;
-    Q3?: string;
-    position: string;
+export interface LapTime {
+    time: number;
+    timestamp: number;
+    track: Track;
+    car: Car;
 }
 
-export interface RaceResult extends Results {
-    race: RaceDetails;
-}
-
-export interface Results {
-    driverId: string;
-    constructorId: string;
-}
-
-export interface RaceDetails {
-    number: string;
-    position: string;
-    points: string;
-    grid: string;
-    laps: string;
-    status: string;
-    Time?: Time;
-    FastestLap?: FastestLap;
-}
-
-export interface Race {
-    season: string;
-    round: string;
-    url: string;
-    raceName: string;
-    Circuit: Circuit;
-    date: string;
-    time: string;
-    QualifyingResults?: QualifyingResult[];
-    Results?: RaceDetails[];
-}
-
-
-export interface Driver {
-    driverId: string;
-    permanentNumber: string;
-    code: string;
-    url: string;
-    givenName: string;
-    familyName: string;
-    dateOfBirth: string;
-    nationality: string;
-}
-
-export interface Constructor {
-    constructorId: string;
-    url: string;
+export interface Track {
     name: string;
-    nationality: string;
+    lenght: number;
 }
 
-export interface Circuit {
-    circuitId: string;
-    circuitName: string;
-    url: string;
-    Location: Location;
-}
-
-export interface Location {
-    country: string;
-    lat: string;
-    locality: string;
-    long: string;
-}
-
-export interface Time {
-    millis?: string;
-    time: string;
-}
-
-export interface FastestLap {
-    rank: string;
-    lap: string;
-    time: Time;
-    averageSpeed: AverageSpeed;
-}
-
-export interface AverageSpeed {
-    units: string;
-    speed: string;
+export interface Car {
+    name: string;
 }
--- src/app/models/query.model.ts
+++ src/app/models/query.model.ts
@@ -1,12 +1,9 @@
 // Objects used by the "Query system"
 export interface DataRequest {
-    seasonId: number;
-    driverId?: string;
-    circuitId?: number;
     dataSources?: DataSources;
 }
 
 export interface DataSources {
-    apiPath: string;
+    cachePath: string;
     dbPath: string;
 }
