9ce7ad3e237f00729c3c5af03c440fddac0b1f8a
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-05-21 19:03:31 +0200

    Modified canActivate method to take in consideration SSR

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from "@angular/core";
+import { Injectable, PLATFORM_ID, Inject } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
+import { isPlatformBrowser } from '@angular/common';
 
 import { AngularFireAuth } from "@angular/fire/auth";
 import { User } from "@firebase/auth-types";
 
-import { Observable } from "rxjs";
-import { tap, map, take } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { tap, map, filter, switchMap } from "rxjs/operators";
 
 
 
 @Injectable()
 export class AuthService implements CanActivate {
 
-	constructor(private afAuth: AngularFireAuth, private router: Router) { }
+	constructor(
+		private afAuth: AngularFireAuth,
+		private router: Router,
+		@Inject(PLATFORM_ID) private platformId) { }
 
 	/**
 	 * This method returns the user data
@@ -30,15 +34,29 @@ export class AuthService implements CanActivate {
 	/**
 	 * This method will say if user can load the main path (and its' children)
 	 */
-	canActivate() {
-		return this.afAuth.authState
-			.pipe(take(1))
-			.pipe(map(authState => !!authState))
-			.pipe(tap(authenticated => {
-				if (!authenticated) {
-					this.router.navigate(["/login"]);
+	public canActivate() {
+		const isBrowser = isPlatformBrowser(this.platformId);
+
+		return this.afAuth.authState.pipe(
+			tap((authState) => {
+				if (!isBrowser) {
+					console.log(`[AccountGuard] Blocking guard in server: authState is always null`);
 				}
-			}));
+			}),
+			filter((_) => isBrowser),
+			tap((authState) => console.log(`[AccountGuard] Checking authState...`)),
+			switchMap(authState => {
+				if (authState) {
+					// Return true if the authState is defined
+					return of(true);
+				}
+
+				// Otherwise, redirect user to the login path and return false
+				console.log(`No permission to access this page! Redirecting to /login`);
+				this.router.navigate(["/login"]);
+				return of(false);
+			})
+		);
 	}
 
 	/**
