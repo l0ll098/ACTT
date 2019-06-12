c679c0a6f6c9785a1306afd22e140928b7028e00
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-06-12 12:33:38 +0200

    The number of cols and row height change based on platform

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
 
 export interface Tile {
     color: string;
@@ -14,14 +15,37 @@ export interface Tile {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
+    private isDesktop: boolean;
+
+    private COLS_ON_DESKTOP = 8;
+    private COLS_ON_MOBILE = 4;
+    private ROWS_HEIGHT_DESKTOP = "192px";
+    private ROWS_HEIGHT_MOBILE = "128px";
+
     tiles: Tile[] = [
         { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
         { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
         { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
         { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+        { text: "Five", cols: 1, rows: 3, color: "blue" },
+        { text: "Six", cols: 3, rows: 1, color: "red" },
+        { text: "Seven", cols: 3, rows: 2, color: "green" },
+        { text: "8", cols: 2, rows: 1, color: "yellow" }
     ];
 
-    calcGridCols(){
-        return 4;
+    constructor(private platform: Platform) {
+        if (this.platform.ANDROID || this.platform.IOS) {
+            this.isDesktop = false;
+        } else {
+            this.isDesktop = true;
+        }
+    }
+
+    calcGridCols() {
+        return this.isDesktop ? this.COLS_ON_DESKTOP : this.COLS_ON_MOBILE;
+    }
+
+    calcGridRowHeight() {
+        return this.isDesktop ? this.ROWS_HEIGHT_DESKTOP : this.ROWS_HEIGHT_MOBILE;
     }
 }
