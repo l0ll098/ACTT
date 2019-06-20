dae9b4f056cce5a0440627f8bc7df0e084e57a84
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-06-20 18:21:53 +0200

    Smaller fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,13 @@
 switch (process.env.FUNCTION_NAME) {
     case "ssr":
         exports.ssr = require("./triggers/ssr");
+        break;
     case "api":
         exports.api = require("./triggers/api");
         break;
     default:
-        console.log(`Unrecognized function '${process.env.FUNCTION_NAME}'`);
+        console.log(`process.env.FUNCTION_NAME was not recognized. Exporting all functions`);
+
+        exports.ssr = require("./triggers/ssr").ssr;
+        exports.api = require("./triggers/api").api;
 }
