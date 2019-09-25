3c29543d58a10ca7704e33c0f28d90362ade5dde
Lorenzo Montanari <lorenzo0.montanari@gmail.com>
2019-09-25 15:30:31 +0200

    Functions are now webpacked

--- functions/src/triggers/ssr.ts
+++ functions/src/triggers/ssr.ts
@@ -1,6 +1,6 @@
 import { https } from 'firebase-functions';
 
-const universal = require(`${process.cwd()}/dist/server`).app;
+const universal = require(`../../dist/server`).app;
 
 // SSR function
 export const ssr = https.onRequest(universal);
